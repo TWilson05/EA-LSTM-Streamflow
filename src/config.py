6ac5c3a86801adea518aff75e8cc58ac482f6f30
Tyler Wilson <tb.wilson05@gmail.com>
2026-01-31 16:14:46 -0800

    added model output directory

--- src/config.py
+++ src/config.py
@@ -7,6 +7,7 @@ PROJECT_ROOT = Path(__file__).resolve().parent.parent
 DATA_DIR = PROJECT_ROOT / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
 PROCESSED_DATA_DIR = DATA_DIR / "processed"
+OUTPUT_DATA_DIR = DATA_DIR / "output"
 MODELS_DIR = PROJECT_ROOT / "models"
 
 ### RAW DATA
@@ -41,5 +42,5 @@ OUTPUT_GLACIER_VOL = PROCESSED_DATA_DIR / "glacier_volume_change.csv"
 
 
 # Create directories if they don't exist
-for path in [DATA_DIR, RAW_DATA_DIR, PROCESSED_DATA_DIR, MODELS_DIR, ERA5_PRECIP_DIR, ERA5_TEMP_DIR, CLIMATE_OUTPUT_DIR, ELEVATION_DIR]:
+for path in [DATA_DIR, RAW_DATA_DIR, PROCESSED_DATA_DIR, MODELS_DIR, ERA5_PRECIP_DIR, ERA5_TEMP_DIR, CLIMATE_OUTPUT_DIR, ELEVATION_DIR, OUTPUT_DATA_DIR]:
     path.mkdir(parents=True, exist_ok=True)
